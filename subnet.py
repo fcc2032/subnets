24f7af63ae9a834e0d3d945ebec7035fadd0a50b
fcc2032 <fcc2032@nyu.edu>
2024-03-10 16:23:46 -0400

    first commit

--- subnet.py
+++ subnet.py
@@ -1,1 +1,75 @@
-import os
+#enter your unique code
+uniquecode = 95645
+
+#problem 1
+p1Network = '107.241.98.16'
+p1Broadcast = '107.241.98.31'
+p1First = '107.241.98.17'
+p1Last =  '107.241.98.30'
+
+#problem 2
+p2Network = '18.227.60.64'
+p2Broadcast = '18.227.60.127'
+p2First = '18.227.60.65'
+p2Last = '18.227.60.126'
+
+#problem 3
+p3Network = '185.212.22.112'
+p3Broadcast = '185.212.22.119'
+p3First = '185.212.22.113'
+p3Last = '185.212.22.118'
+
+#problem 4
+p4Network = '96.197.240.128'
+p4Broadcast = '96.197.240.191'
+p4First = '96.197.240.129'
+p4Last = '96.197.240.190'
+
+#problem 5
+p5Network = '7.182.201.0'
+p5Broadcast = '7.182.201.255'
+p5First = '7.182.201.1'
+p5Last = '7.182.201.254'
+
+#problem 6
+p6Network = '174.167.163.224'
+p6Broadcast = '174.167.163.255'
+p6First = '174.167.163.225'
+p6Last =  '174.167.163.254'
+
+#problem 7
+p7Network = '85.152.125.0'
+p7Broadcast = '85.152.125.127'
+p7First = '85.152.125.1'
+p7Last = '85.152.125.126'
+
+#problem 8
+p8Network = '252.137.87.64'
+p8Broadcast = '252.137.87.79'
+p8First = '252.137.87.65'
+p8Last = '252.137.87.78'
+
+#problem 9
+p9Network = '163.122.48.64'
+p9Broadcast = '163.122.48.127'
+p9First = '163.122.48.65'
+p9Last = '163.122.48.126'
+
+#problem 10
+p10Network = '74.108.10.152'
+p10Broadcast = '74.108.10.159'
+p10First = '74.108.10.153'
+p10Last = '74.108.10.158'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
